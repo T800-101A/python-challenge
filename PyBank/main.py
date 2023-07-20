4cbe6eb4f91a1a8e6528f2fe6385c36b61b95c52
Arnold <vincent@t-101.com>
2023-07-20 02:44:32 -0500

    PyBank Folder modified
    code updated with analysis txt completed

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,40 +2,56 @@ import os
 import csv
 
 
-csvpath = os.path.join("budget_data.csv")
-csvoutput = os.path.join("resultspybank.txt")
+csvpath = os.path.join("C:/Users/arnol/OneDrive/Documents/SMU/GitSMU/python-challenge/PyBank/Resources/budget_data.csv")
+csvoutput = os.path.join("C:/Users/arnol/OneDrive/Documents/SMU/GitSMU/python-challenge/PyBank/analysis/resultspybank.txt")
 
+
+## Defining Variables set in 0 for then asign their values
 total_months = 0
 net_total_amount = 0
 average_change = 0
 
-
+## Defining lists to save extraction of values from dataset
 net_change_list = []
 months = []
 greatest_increase=["",-1000]
 greatest_decrease = ["", 999999]
 
 
-
+## opening the file as csvfile from the already assigned csvpath,reading it and eliminating headers 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
     csv_headers = next(csvreader)
+    
+    
+##total_months will be adding till last iteration
+##this lists will be set with initial positions and values in it to then save data
+##to then be use in the formulas to obtain total results
 
     total_months += 1
     first_row = next(csvreader)
-
     prev = int(first_row[1])
-
-
     net_total_amount += int(first_row[1])
+    
+##iteration into the data
 
     for x in csvreader:
+        
+##extracting data from index 0 (dates) into months variable.        
         months.append(x[0])
         total_months += 1
+        
+#extracting and adding while iteraring into index 1 (prof/loss) to get totals.
         net_total_amount += int(x[1])
+        
+##calculating the net changes and saving this result in a new list to then comparate them and save the data
+##of greatest increase/decrease saving the biggest and lowest with if conditionals
+        
         net_change = int(x[1]) - prev
         net_change_list.append(net_change)
         prev = int(x[1])
+        
+        
         if net_change > greatest_increase[1]:
             greatest_increase[0]=x[0]
             greatest_increase[1]=net_change
@@ -44,33 +60,35 @@ with open(csvpath) as csvfile:
             greatest_decrease[1]=net_change
 
 
-
+##utilazing now out of the loop the list created (net_change_list) to calculate averages
 
     average_change = sum(net_change_list) / len(net_change_list)
 
-
-    print("Financial Analysis")
+##then printf to show the results
+    
+    print("\nFinancial Analysis")
     print(f"-"*20)
-    print(f"total number of months is : {total_months}")
-    print(f"net total amount is : ${net_total_amount}")
-    print(f"average change is {str(average_change)}")
-    print(f"greatest increase in profit : {greatest_increase[0]} {greatest_increase[1]}")
-    print(f"greates decrease in profit : {greatest_decrease[0]} {greatest_decrease[1]}")
+    print(f"Total months                 :   {total_months} \n")
+    print(f"Total                        : $ {net_total_amount}")
+    print(f"Average Change               : ${round(average_change,2)}\n")
+    print(f"Greatest Increase in Profit  : {greatest_increase[0]}  {greatest_increase[1]}")
+    print(f"Greatest Decrease in Profit  : {greatest_decrease[0]} {greatest_decrease[1]}")
+    
+    
+    with open(csvoutput, "a") as txtfile:
+        txtfile.write(
+f"""
+\n                                Financial Analysis \n
+                {"-"*50}
 
-    for x in csvreader:
-
-        with open(csvoutput, "a") as txtfile:
-            txtfile.write(
-                         f"Financial Analysis \n"
-                        
-                         f"total number of months is : {total_months} \n"
-                         f"net total amount is : ${net_total_amount} \n"
-                         f"average change is {str(average_change)} \n"
-                         f"greatest increase in profit : {greatest_increase[0]} {greatest_increase[1]} \n"
-                         f"greates decrease in profit : {greatest_decrease[0]} {greatest_decrease[1]} \n"
-                         f"title: {x[0]} \n"
-                         f"rating_level: {x[2]} \n"
-                         f"user_rating: {x[5]} \n"
-                         )
+                Total months                 :   {total_months} \n
+                Total                        : $ {net_total_amount}\n
+                Average Change               : ${round(average_change,2)}\n
+                Greatest Increase in Profit  : {greatest_increase[0]}  {greatest_increase[1]}\n
+                Greatest Decrease in Profit  : {greatest_decrease[0]} {greatest_decrease[1]}\n
+                {"-"*50}
 
 
+                             """) 
+        
+        
